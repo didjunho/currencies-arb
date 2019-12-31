7517d545f3c54e1f7896b5df4eca4172d6f265d6
Monica Wang <moniwang@umich.edu>
2019-12-31 00:43:03 -0500

    showing currency values in table form

--- code/driver.py
+++ code/driver.py
@@ -6,15 +6,17 @@ currencies = ["CAD","HKD","ISK","PHP","DKK","HUF","CZK","GBP","RON","SEK","IDR",
 "INR","BRL","RUB","HRK","JPY","THB","CHF","EUR","MYR","BGN","TRY","CNY","NOK",
 "NZD","ZAR","USD","MXN","SGD","AUD","ILS","KRW","PLN"]
 
-def getCurrencies(base='USD', print=False):
+def getCurrencies(base='USD', printout=False):
     '''Recieves relavent list of currencies written in USD as a base.'''
     get_url = "https://api.exchangeratesapi.io/latest?base=" + base
     out = "../data/data_" + base + ".txt"
     with urllib.request.urlopen(get_url) as url:
         data = json.loads(url.read().decode())
         with open(out, 'w') as outfile:
+            if base == 'EUR':
+                data['rates']['EUR'] = 1
             json.dump(data['rates'], outfile)
-        if(print):
+        if(printout):
             readCurrencies(base)
 
 def readCurrencies(base='USD'):
@@ -36,7 +38,7 @@ def startCurrencyThreads():
     dt_string = now.strftime("%B %d, %Y %H:%M:%S")
     out = "../data/data_time.txt"
     with open(out, 'w+') as outfile:
-        outfile.write(dt_string)
+        outfile.write('{"time":"' + dt_string + '"}')
     for thread in threads:
         thread.start()
     for thread in threads:
